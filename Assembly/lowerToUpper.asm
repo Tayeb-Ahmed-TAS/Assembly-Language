30b3af7b88c247182631fd9d975753d01a9d36ee
Tayeb Ahmed <104755858+Tayeb-Ahmed-TAS@users.noreply.github.com>
2024-03-19 10:17:37 +0600

    Add files via upload

---
+++ Assembly/lowerToUpper.asm
@@ -0,0 +1,55 @@
+;Convert lowercase to uppercase 
+
+.model small
+.stack 100h
+
+.data
+
+msg1 db 0dh,0ah,'Enter an Input',0dh,0ah,'$'
+msg2 db 0dh,0ah,'The Output is',0dh,0ah,'$'
+
+.code
+
+main proc
+    
+    mov ax,@data
+    mov ds,ax
+    
+    lea dx,msg1
+    mov ah,09h
+    int 21h
+    
+     ; Read the character
+    mov ah,01h
+    int 21h
+    
+     ; Convert to uppercase (ASCII manipulation)
+    
+    cmp al,'a' ; Jump if less than 'a' (not lowercase)
+    jl endc    ; jg means Jump If Less Than
+    
+    cmp al,'z' ; Jump if greater than 'z' (not lowercase)
+    jg endc    ; jg means Jump If Greater Than
+    
+    sub al,32  ; Convert to uppercase by subtracting 32
+    mov bl,al
+    
+    endc:
+    
+    lea dx,msg2
+    mov ah,09h
+    int 21h
+    
+     ; Display the converted character
+    
+    mov dl,bl
+    mov ah,2h
+    int 21h
+    
+     ; Exit the program
+    
+    mov ah,4ch
+    int 21h
+    
+    main endp
+end main
---
+++ Assembly/upperToLowerUsingMacro.asm
@@ -0,0 +1,52 @@
+;Convert uppercase to lowecase using macro 
+
+.model small
+.stack 100h
+
+print macro msg
+    
+    lea dx,msg
+    mov ah,09h
+    int 21h
+    
+endm
+
+.data
+
+msg1 db 0dh,0ah,'Enter an Input',0dh,0ah,'$'
+msg2 db 0dh,0ah,'The Output is',0dh,0ah,'$'
+
+.code
+
+main proc
+    
+    mov ax,@data
+    mov ds,ax
+    
+    print msg1
+    
+    mov ah,01h
+    int 21h
+    
+    cmp al,'A'
+    jl endc
+    
+    cmp al,'Z'
+    jg endc
+    
+    add al,32
+    mov bl,al
+    
+    endc:
+    
+    print msg2
+    
+    mov dl,bl
+    mov ah,2h
+    int 21h
+    
+    mov ah,4ch
+    int 21h
+    
+    main endp
+end main
